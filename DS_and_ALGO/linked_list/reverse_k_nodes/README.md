bf6b7d20b1000e15bfffe6da635a85ae28e71f6e
vineet kumar <vineetkr98@gmail.com>
2022-08-03 14:31:57 +0530

    (feat) Reverse k nodes of linked list

---
+++ DS_and_ALGO/linked_list/reverse_k_nodes/README.md
@@ -0,0 +1,59 @@
+**Reverse k nodes**
+
+You are given a Singly Linked List of integers and an integer array 'B' of size 'N'. Each element in the array 'B' represents a block size. Modify the linked list by reversing the nodes in each block whose sizes are given by the array 'B'.
+
+**Note**
+1) If you encounter a situation when 'B[i]' is greater than the number of remaining nodes in the list, then simply reverse the remaining nodes as a block and ignore all the block sizes from 'B[i]'.
+
+2) All block sizes are contiguous i.e. suppose that block 'B[i]' ends at a node cur, then the block 'B[i+1]' starts from the node just after the node cur.
+
+**Example**\
+Linked list: 1->2->3->4->5\
+Array B: 3 3 5\
+Output: 3->2->1->5->4
+
+We reverse the first block of size 3 and then move to block 2. Now, since the number of nodes remaining in the list (2) is less than the block size (3), we reverse the remaining nodes (4 and 5) as a block and ignore all the block sizes that follow.
+
+**INPUT FORMAT**\
+The first line of the input contains the elements of the singly linked list separated by a single space and terminated by -1. Hence, -1 would not be a list element.
+
+The second line contains a single integer 'N', denoting the size of the block array 'B'.
+
+The third line contains 'N' single space-separated elements of the block array 'B'.
+
+**OUTPUT FORMAT**\
+You should return the modified linked list where elements should be single-space separated, terminated by -1.
+
+**Note**\
+You don't need to print the output, it has already been taken care of. Just implement the given function.
+
+**CONSTRAINTS**\
+0 <= L <= 5 * 10^5\
+-10^9 <= data <= 10^9 and data != -1\
+1 <= N <= 5 * 10^5\
+0 <= B[i] <= 5 * 10^5\
+Where 'L' is the number of nodes in the linked list and 'data' is the value of a node in the linked list. \
+Time Limit: 1 sec
+
+**SAMPLE INPUT 1**
+```
+1 2 3 4 5 6 7 8 9 10 11 -1
+3
+2 3 4
+```
+**SAMPLE OUTPUT 1**\
+6 0 5 1 -1
+
+**SAMPLE INPUT 2**
+```
+1 2 3 4 5 -1
+10
+1 0 0 1 0 0 1 1 1 0
+```
+
+**SAMPLE OUTPUT 2**\
+1 2 3 4 5 -1
+
+**TIME COMPLEXITY**\
+T(n) = O(b*n^2) where n is length of linked list and b is size of block array.
+
---
+++ DS_and_ALGO/linked_list/reverse_k_nodes/reverse_k_nodes.cpp
@@ -0,0 +1,233 @@
+#include <iostream>
+
+class linked_list {
+private:
+  int data;
+  linked_list *next = NULL;
+
+public:
+  linked_list(int data) { this->data = data; }
+
+  int get_data() const { return this->data; }
+
+  void set_next(linked_list *next) { this->next = next; }
+
+  linked_list *get_next() const { return this->next; }
+
+  static linked_list *take_input() {
+    int data = 0;
+    std::cin >> data;
+    linked_list *head = NULL, *tail = NULL;
+    while (data != -1) {
+      linked_list *node = new linked_list(data);
+      if (head == NULL) {
+        head = node;
+        tail = node;
+      } else {
+        tail->set_next(node);
+        tail = node;
+      }
+      std::cin >> data;
+    }
+    return head;
+  }
+
+  static int length(linked_list *head) {
+    // Non recursively
+    int len = 0;
+    while (head != NULL) {
+      head = head->get_next();
+      ++len;
+    }
+    return len;
+
+    // Recursively
+    // if (head == NULL)
+    //   return 0;
+
+    // return 1 + linked_list::length(head->get_next());
+  }
+
+  static void print(linked_list *head) {
+    while (head != NULL) {
+      std::cout << head->get_data() << " ";
+      head = head->get_next();
+    }
+    std::cout << std::endl;
+  }
+
+  static void print_node(linked_list *head, int index) {
+    int i = 0;
+    while (head != NULL) {
+      if (i == index) {
+        std::cout << head->get_data() << std::endl;
+        break;
+      }
+      head = head->get_next();
+      ++i;
+    }
+    return;
+  }
+
+  static linked_list *insert(linked_list *head, int const &index, int data) {
+    linked_list *temp = head;
+    int len = linked_list::length(temp);
+    if (index < 0 || index > len)
+      return temp;
+
+    for (int i = 0; i < index - 1; ++i)
+      temp = temp->get_next();
+
+    linked_list *new_node = new linked_list(data);
+
+    if (index == 0) {
+      new_node->set_next(temp);
+      return new_node;
+    } else if (index == len) {
+      temp->set_next(new_node);
+    } else {
+      linked_list *temp1 = temp->get_next();
+      temp->set_next(new_node);
+      new_node->set_next(temp1);
+    }
+    return head;
+  }
+
+  static linked_list *delete_node(linked_list *head, int const &index) {
+    linked_list *temp = head;
+    int len = linked_list::length(temp);
+    if (index < 0 || index >= len)
+      return head;
+
+    for (int i = 0; i < index - 1; ++i)
+      temp = temp->get_next();
+
+    if (index == 0) {
+      linked_list *new_head = temp->get_next();
+      delete temp;
+      return new_head;
+    } else if (index == len - 1) {
+      delete temp->get_next();
+      temp->set_next(NULL);
+    } else {
+      linked_list *delete_node = temp->get_next();
+      temp->set_next(delete_node->get_next());
+      delete delete_node;
+    }
+    return head;
+  }
+
+  void static delete_all(linked_list *head) {
+    while (head != NULL) {
+      linked_list *new_head = head->get_next();
+      delete head;
+      head = new_head;
+    }
+    return;
+  }
+
+  static linked_list *insert_recursively(linked_list *head, int start_index,
+                                         int index, int data) {
+
+    if (start_index == index) {
+      linked_list *new_node = new linked_list(data);
+      new_node->set_next(head);
+      return new_node;
+    } else if (head == NULL)
+      return NULL;
+
+    linked_list *small_ans = linked_list::insert_recursively(
+        head->get_next(), start_index + 1, index, data);
+    head->set_next(small_ans);
+    return head;
+  }
+
+  static linked_list *delete_recursively(linked_list *head, int start_index,
+                                         int index) {
+    if (head == NULL || start_index > index)
+      return head;
+    else if (start_index == index) {
+      linked_list *delete_node_next = head->get_next();
+      delete head;
+      return delete_node_next;
+    }
+
+    linked_list *small_ans = linked_list::delete_recursively(
+        head->get_next(), start_index + 1, index);
+    head->set_next(small_ans);
+    return head;
+  }
+
+  static int find_node(linked_list *head, int start_index, int data) {
+    if (!head)
+      return -1;
+    else if (head->get_data() == data)
+      return start_index;
+
+    return linked_list::find_node(head->get_next(), start_index + 1, data);
+  }
+
+  static linked_list *reverse_linked_list(linked_list *head) {
+    if (!head || !head->get_next())
+      return head;
+    linked_list *ans = reverse_linked_list(head->get_next());
+
+    linked_list *temp = ans;
+    while (temp->get_next())
+      temp = temp->get_next();
+
+    head->set_next(NULL);
+    temp->set_next(head);
+    return ans;
+    //TODO: Improve time complexity from O(n^2) to O(n)
+  }
+};
+
+linked_list *getListAfterReverseOperation(linked_list *head, int const &b_size,
+                                          int const *const b) {
+  int len = linked_list::length(head);
+  int ele_to_skip = 0;
+  linked_list *subset_head = head;
+  linked_list *prev_subset_tail = NULL;
+  for (int i = 0; i < b_size; ++i) {
+    if (ele_to_skip >= len)
+      break;
+    else if (b[i] == 0)
+      continue;
+    ele_to_skip += b[i];
+
+    linked_list *subset_tail = subset_head;
+    for (int j = 0; j < b[i] - 1 && subset_tail->get_next(); ++j)
+      subset_tail = subset_tail->get_next();
+
+    linked_list *subset_tail_next = subset_tail->get_next();
+    // Reverse subset
+    subset_tail->set_next(NULL);
+    linked_list *ans = linked_list::reverse_linked_list(subset_head);
+    if (i == 0)
+      head = ans;
+    if (prev_subset_tail)
+      prev_subset_tail->set_next(ans);
+    subset_head->set_next(subset_tail_next);
+    prev_subset_tail = subset_head;
+    subset_head = subset_tail_next;
+  }
+  return head;
+}
+
+int main() {
+  int block_reverse_size = 0;
+  linked_list *head = linked_list::take_input();
+  std::cin >> block_reverse_size;
+  int *block_reverses = new int[block_reverse_size];
+  for (int i = 0; i < block_reverse_size; ++i)
+    std::cin >> block_reverses[i];
+
+  head = getListAfterReverseOperation(head, block_reverse_size, block_reverses);
+  linked_list::print(head);
+
+  delete[] block_reverses;
+  linked_list::delete_all(head);
+
+  return 0;
+}
