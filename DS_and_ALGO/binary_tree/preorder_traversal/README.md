ad4f74c1d1723ed6d256119ccb7b9afbbc4a7e39
vineet kumar <vineetkr98@gmail.com>
2022-08-31 19:07:00 +0530

    (feat) Preorder traversal of binary tree

---
+++ DS_and_ALGO/binary_tree/preorder_traversal/README.md
@@ -0,0 +1,28 @@
+**Preorder Binary Tree**
+
+You are given the root node of a binary tree.Print its preorder traversal.
+
+**INPUT FORMAT**\
+The first and the only line of input will contain the node data, all separated by a single space. Since -1 is used as an indication whether the left or right node data exist for root, it will not be a part of the node data.
+
+**OUTPUT FORMAT**\
+The only line of output prints the preorder traversal of the given binary tree.
+
+**CONSTRAINTS**\
+1 <= N <= 10<sup>6</sup>\
+Where N is the total number of nodes in the binary tree.\
+Time Limit: 1 sec
+
+**SAMPLE INPUT**
+```
+1 2 3 4 5 6 7 -1 -1 -1 -1 -1 -1 -1 -1
+```
+
+**SAMPLE OUTPUT**\
+1 2 4 5 3 6 7
+
+**TIME COMPLEXITY**\
+T(n) = O(n) where n is number of nodes in binary tree
+
+**SPACE COMPLEXITY**\
+S(n) = O(h) where h is height of binary tree
---
+++ DS_and_ALGO/binary_tree/preorder_traversal/preorder_traversal.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <queue>
+
+template <typename T> class binary_tree {
+  T data;
+  binary_tree<T> *left_node;
+  binary_tree<T> *right_node;
+
+public:
+  binary_tree(T data) {
+    this->data = data;
+    left_node = NULL;
+    right_node = NULL;
+  }
+
+  void update_data(T data) { this->data = data; }
+
+  T get_data() const { return data; }
+
+  void update_left_node(binary_tree<T> *const left_node) {
+    this->left_node = left_node;
+  }
+
+  void update_right_node(binary_tree<T> *const right_node) {
+    this->right_node = right_node;
+  }
+
+  binary_tree<T> *get_left_node() const { return left_node; }
+  binary_tree<T> *get_right_node() const { return right_node; }
+
+  static binary_tree<T> *take_input_levelwise() {
+    int root_data = -1;
+    std::cin >> root_data;
+    if (root_data == -1)
+      return NULL;
+    binary_tree<T> *root = new binary_tree<T>(root_data);
+    std::queue<binary_tree<T> *> q;
+    q.push(root);
+
+    while (!q.empty()) {
+      binary_tree<T> *curr_node = q.front();
+      q.pop();
+      int left_child = -1, right_child = -1;
+      std::cin >> left_child;
+      if (left_child != -1) {
+        binary_tree<T> *left_node = new binary_tree<T>(left_child);
+        curr_node->update_left_node(left_node);
+        q.push(left_node);
+      }
+
+      std::cin >> right_child;
+      if (right_child != -1) {
+        binary_tree<T> *right_node = new binary_tree<T>(right_child);
+        curr_node->update_right_node(right_node);
+        q.push(right_node);
+      }
+    }
+    return root;
+  }
+
+  static void preorder_traversal(binary_tree<T> *root) {
+    if (!root)
+      return;
+
+    std::cout << root->data << " ";
+    binary_tree<T>::preorder_traversal(root->get_left_node());
+    binary_tree<T>::preorder_traversal(root->get_right_node());
+  }
+
+  ~binary_tree() {
+    delete left_node;
+    delete right_node;
+  }
+};
+
+int main() {
+  binary_tree<int> *root = binary_tree<int>::take_input_levelwise();
+  binary_tree<int>::preorder_traversal(root);
+  delete root;
+  return 0;
+}
